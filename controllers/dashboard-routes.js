b1583d5e987b98de44d6639d2def555936f11812
BJ Smith <bryanjeremysmith@gmail.com>
2023-02-13 18:38:42 -0800

    adding in dashboard routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { Post, User, Comment} = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+        });
+
+        const posts = postData.map((post) => post.get({plain: true }));
+
+        res.render('all-posts-admin', {
+            layout: 'dashboard',
+            posts,
+        });
+    } 
+    catch (err){
+        res.redirect('login');
+    }
+});
+
+router.get('/new', withAut, (req, res) => {
+    res.render('new-post', {
+        layout: 'dashboard',
+    })
+})
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try{
+        const postData = await Post.findByPk(req.params.id);
+
+        if(postData) {
+            const post = postData.get({plain: true})
+
+            res.render('edit-post', {
+                layout: 'dashboard',
+                post
+            })
+        }else{
+            res.status(404).json('No post data found!');
+        }
+    }catch (err){
+        res.redirect('login');
+    }
+})
+
+module.exports = router;
